4cb640714f072334086ef54049aeccb046fdbfd0
David Stotijn <dstotijn@gmail.com>
2020-04-25 01:26:21 +0200

    Remove `docker-compose.test.yml`, just use `docker` in test script

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
-export POSTGRES_DSN="postgres://ct-diag-test:ct-diag-test@127.0.0.1:54321/ct-diag-test?sslmode=disable"
+NAME="pg-ct-diag-test"
+PORT="54321"
 
-docker-compose -f docker-compose.test.yml up -d
-go test ./... -v
-docker-compose -f docker-compose.test.yml stop postgres-test
+export POSTGRES_DSN="postgres://$NAME:$NAME@127.0.0.1:$PORT/$NAME?sslmode=disable"
+
+docker run --rm --name $NAME -d -p $PORT:5432 \
+  -e POSTGRES_USER=$NAME \
+  -e POSTGRES_PASSWORD=$NAME \
+  -v $PWD/postgres/schema.sql:/docker-entrypoint-initdb.d/schema.sql \
+  postgres:11.7-alpine
+
+go test ./... -count=1
+
+docker rm -f $NAME 
