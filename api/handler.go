0144ec602a7125bfa0dd3d08644fda0af3c145c7
Arian van Putten <arian.vanputten@gmail.com>
2020-04-28 10:32:28 +0200

    Add prometheus metrics
    This will add default metrics like amount of http requests per second,
    and go runtime statistics

--- api/handler.go
+++ api/handler.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 
 	"github.com/dstotijn/ct-diag-server/diag"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 type handler struct {
@@ -19,9 +20,9 @@ func NewHandler(repo diag.Repository) http.Handler {
 	h := handler{diagSvc: diag.NewService(repo)}
 
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("/diagnosis-keys", h.diagnosisKeys)
 	mux.HandleFunc("/health", h.health)
+	mux.Handle("/metrics", promhttp.Handler())
 
 	return mux
 }
