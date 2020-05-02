1a170910fbac619066d50706b77d9925016dbad8
David Stotijn <dstotijn@gmail.com>
2020-05-02 17:42:33 +0200

    Use timestamp with time zone for `created_at`

--- db/postgres/schema.sql
+++ db/postgres/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE diagnosis_keys
 (
     key bytea NOT NULL,
     interval_number bigint NOT NULL, -- We don't really need 64 bytes, but uint32's range doesn't fit in `integer`
-    created_at timestamp without time zone NOT NULL,
+    created_at timestamp with time zone NOT NULL,
     CONSTRAINT diagnosis_keys_pkey PRIMARY KEY (key)
 );
 
--- scripts/purge.sh
+++ scripts/purge.sh
@@ -6,5 +6,5 @@ INTERVAL=$2
 
 psql -v ON_ERROR_STOP=1 $DSN <<-EOSQL
     DELETE FROM diagnosis_keys
-    WHERE created_at < CURRENT_TIME - interval '$INTERVAL';
+    WHERE created_at < current_timestamp - interval '$INTERVAL';
 EOSQL
