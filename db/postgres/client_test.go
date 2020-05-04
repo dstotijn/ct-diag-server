11cb41f1923aad66346ebbb822e61fc4dde3b629
David Stotijn <dstotijn@gmail.com>
2020-05-05 01:44:00 +0200

    Fix failing test

--- db/postgres/client_test.go
+++ db/postgres/client_test.go
@@ -40,7 +40,7 @@ func TestMain(m *testing.M) {
 func TestStoreDiagnosisKeys(t *testing.T) {
 	ctx := context.Background()
 	key := [16]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	uploadedAt := time.Unix(42, 0)
+	uploadedAt := time.Unix(42, 0).UTC()
 
 	tests := []struct {
 		name        string
